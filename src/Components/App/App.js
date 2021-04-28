88d5d0ff1ef3c40a1298e4ad140945ce6a1f0321
SykaBliet <aurele.guitard@gmail.com>
2021-04-28 12:42:14 +0200

    Task 46: bug cant add track to playlist

---
+++ src/Components/App/App.js
@@ -0,0 +1,84 @@
+// import logo from './logo.svg';
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'name1',
+          artist: 'artist1',
+          album: 'album1',
+          id: '1'
+        },
+        {
+          name: 'name2',
+          artist: 'artist2',
+          album: 'album2',
+          id: '2'
+        },
+        {
+          name: 'name3',
+          artist: 'artist3',
+          album: 'album3',
+          id: '3'
+        }
+      ],
+      playlistName: 'My Play List Name',
+      playlistTracks: [
+        {
+          name: 'playlistname1',
+          artist: 'playlistartist1',
+          album: 'playlistalbum1',
+          id: '1'
+        },
+        {
+          name: 'playlistname12',
+          artist: 'playlistartist2',
+          album: 'playlistalbum2',
+          id: '2'
+        },
+        {
+          name: 'playlistname3',
+          artist: 'playlistartist3',
+          album: 'playlistalbum3',
+          id: '3'
+        }
+      ]
+    }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults}
+              onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
